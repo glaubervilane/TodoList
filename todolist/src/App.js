fb0bef032367efdc0715a66748da88ce73805f47
Glauber Marques <glauberfmarques@gmail.com>
2023-08-25 22:20:25 -0600

    Creating App in the main part of application and insert styles on that

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles/App.css'
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      ids: 1,
+      text: "Create a new functionality on the system",
+      category: "Work",
+      isCompleted: false,
+    },
+    {
+      ids: 2,
+      text: "Go to the gymn",
+      category: "Personal",
+      isCompleted: false,
+    },
+    {
+      ids: 3,
+      text: "TStudy React",
+      category: "Study",
+      isCompleted: false,
+    },
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Task List</h1>
+      <div className='todo-list'>
+        {todos.map((todo) => (
+          <div className='todo'>
+            <div className='content'>
+              <p>{todo.text}</p>
+              <p className='category'>({todo.category})</p>
+            </div>
+            <div>
+              <button>Complete</button>
+              <button>X</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
